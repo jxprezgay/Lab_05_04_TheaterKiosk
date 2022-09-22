384703f9dd207d317c43a77a074bef98c0e5fce2
jxprezgay <jxprezgay@yahoo.com>
2022-09-22 13:28:19 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 1;
+
+        System.out.println("What is your age: ");
+
+        if (age >= 21){
+            System.out.println("You get a paper wristband!");
+        }
+    }
+}
